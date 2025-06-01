2a142522a0338ef7097a7c45ac5dd13a22ce749a
Cloudyky <ikhwanch131@gmail.com>
2025-06-01 22:27:34 +0800

    update new car management

--- script.js
+++ script.js
@@ -1,78 +1,113 @@
 $(document).ready(function () {
-    let customerCount = 0;
-    const maxCustomers = 10;
-    const customerBrands = ["Porsche", "Audi"];
+  let customerCount = 1;
+  let totalSales = 0;
+  const carPrice = 100000;
 
-    function generateCustomer(n) {
-        const brand = customerBrands[Math.floor(Math.random() * customerBrands.length)];
-        const customer = $("<div>")
-            .addClass("draggable customer")
-            .attr("id", "cust" + n)
-            .attr("draggable", "true")
-            .attr("data-brand", brand.toLowerCase())
-            .text("Customer: " + brand + " " + n);
-        $(".left").append(customer);
+  const audiModels = ["Audi R8", "Audi A4", "Audi Q5", "Audi TT"];
+  const porscheModels = ["Porsche 911", "Porsche Cayenne", "Porsche Taycan", "Porsche Panamera"];
+
+  // Generate cars
+  function generateCars(models, brand) {
+    models.forEach(model => {
+      const carDiv = $('<div>')
+        .addClass('car-with-customer')
+        .attr('draggable', 'true')
+        .attr('data-brand', brand)
+        .attr('data-model', model)
+        .html(`<div class="car">${model}</div>`);
+      $(`#${brand.toLowerCase()}-cars`).append(carDiv);
+    });
+  }
+
+  generateCars(audiModels, 'Audi');
+  generateCars(porscheModels, 'Porsche');
+
+  // Generate customers every 2s until 10
+  const customerInterval = setInterval(() => {
+    if ($('#customer-queue .customer').length < 10) {
+      const brand = Math.random() < 0.5 ? 'Audi' : 'Porsche';
+      const cust = $('<div>')
+        .addClass('customer')
+        .attr('draggable', 'true')
+        .attr('id', 'cust' + customerCount)
+        .text(`Customer ${brand} ${customerCount}`);
+      $('#customer-queue').append(cust);
+      customerCount++;
     }
+  }, 2000);
 
-    const interval = setInterval(() => {
-        if ($(".left .customer").length < maxCustomers) {
-        customerCount++;
-        generateCustomer(customerCount);
+  // Drag start
+  $(document).on('dragstart', '.customer, .car-with-customer', function (e) {
+    e.originalEvent.dataTransfer.setData("text/plain", $(this).attr('id') || $(this).data('model'));
+    e.originalEvent.dataTransfer.setData("type", $(this).hasClass('customer') ? 'customer' : 'car');
+    $(this).addClass("dragging");
+  });
+
+  // Allow drop
+  $('.customer-slot, #payment-area, #exit-area').on('dragover', function (e) {
+    e.preventDefault();
+  });
+
+  // Drop customer into car
+//   $('.customer-slot').on('drop', function (e) {
+//     e.preventDefault();
+//     const dragType = e.originalEvent.dataTransfer.getData("type");
+//     const id = e.originalEvent.dataTransfer.getData("text/plain");
+//     if (dragType === 'customer') {
+//       const cust = $('#' + id);
+//       const targetCar = $(this).find('.car-with-customer:not(:has(.customer))').first();
+//       if (targetCar.length) {
+//         targetCar.append(cust);
+//       }
+//     }
+//   });
+
+    // Drop customer directly into specific car
+    $(document).on('dragover', '.car-with-customer', function (e) {
+    e.preventDefault();
+    });
+
+    $(document).on('drop', '.car-with-customer', function (e) {
+    e.preventDefault();
+    const dragType = e.originalEvent.dataTransfer.getData("type");
+    const id = e.originalEvent.dataTransfer.getData("text/plain");
+
+    if (dragType === 'customer') {
+        const cust = $('#' + id);
+        // Check kalau kereta belum ada customer
+        if ($(this).find('.customer').length === 0) {
+        $(this).append(cust);
         }
-        if (customerCount >= maxCustomers) clearInterval(interval);
-    }, 2000);
-
-    // Dragging customer only
-    $(document).on("dragstart", ".customer", function (e) {
-        const id = $(this).attr("id");
-        e.originalEvent.dataTransfer.setData("text/plain", id);
-        console.log("Dragging customer:", id);
+    }
     });
 
-    // Drop customer to correct brand section
-    $(".customer-drop").on("dragover", function (e) {
-        e.preventDefault();
-    });
+  // Drop to payment
+  $('#payment-area').on('drop', function (e) {
+    e.preventDefault();
+    const dragType = e.originalEvent.dataTransfer.getData("type");
+    const id = e.originalEvent.dataTransfer.getData("text/plain");
+    if (dragType === 'car') {
+      const carDiv = $(`[data-model="${id}"]`);
+      if (carDiv.find('.customer').length) {
+        totalSales += carPrice;
+        $('#sales-amount').text(totalSales.toLocaleString());
+        carDiv.remove();
+      }
+    }
+  });
 
-    $(".customer-drop").on("drop", function (e) {
-        e.preventDefault();
-        const customerId = e.originalEvent.dataTransfer.getData("text/plain");
-        const $customer = $("#" + customerId);
-
-        const brand = $(this).data("brand");
-        if ($customer.data("brand") === brand) {
-        const customerBox = $("<div>")
-            .addClass("customer-box")
-            .attr("draggable", "true")
-            .attr("id", "pair" + customerId)
-            .text("🚗 " + brand.toUpperCase() + " + " + $customer.text());
-
-        $(this).closest(".brand-drop").append(customerBox);
-        $customer.remove();
-
-        // Topup customer
-        if ($(".left .customer").length < maxCustomers) {
-            customerCount++;
-            generateCustomer(customerCount);
-        }
-        } else {
-            alert("Customer only wants a " + $customer.data("brand"));
-        }
-    });
-
-    // Drag full car+customer box to payment
-    $(document).on("dragstart", ".customer-box", function (e) {
-        e.originalEvent.dataTransfer.setData("text/plain", $(this).attr("id"));
-    });
-
-    $(".payment-drop").on("dragover", function (e) {
-        e.preventDefault();
-    });
-
-    $(".payment-drop").on("drop", function (e) {
-        e.preventDefault();
-        const boxId = e.originalEvent.dataTransfer.getData("text/plain");
-        const $box = $("#" + boxId);
-        $(".payment-drop").append($box);
-    });
+  // Drop to exit
+  $('#exit-area').on('drop', function (e) {
+    e.preventDefault();
+    const dragType = e.originalEvent.dataTransfer.getData("type");
+    const id = e.originalEvent.dataTransfer.getData("text/plain");
+    if (dragType === 'customer') {
+      $('#' + id).remove();
+    } else if (dragType === 'car') {
+      const carDiv = $(`[data-model="${id}"]`);
+      if (carDiv.find('.customer').length) {
+        carDiv.find('.customer').remove();
+      }
+    }
+  });
 });
